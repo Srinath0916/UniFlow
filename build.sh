6a8db2a5e2995d4bca8ad1547737ee5d50b057e3
Srinath0916 <srinath0916@gmail.com>
2026-03-18 22:23:48 +0530

    auto-create superuser in build script

--- build.sh
+++ build.sh
@@ -4,3 +4,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+
+# Create superuser automatically if it doesn't exist
+python manage.py shell << 'EOF'
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@uniflow.com', 'project123')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+EOF
